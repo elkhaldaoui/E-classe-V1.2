4d889d708846cecd47269b060f868ed7acefe920
Oussama Elkhaldaoui <93930380+elkhaldaoui@users.noreply.github.com>
2022-02-02 09:40:56 +0100

    date

--- edite.php
+++ edite.php
@@ -1,5 +1,13 @@
 <?php
+$id = $_GET['id'];
 include 'assets/navbar.php';
+$connection = mysqli_connect ("localhost","root","","e_classe_db");
+$queery = "SELECT * FROM students WHERE id=$id ";
+$results = mysqli_query($connection,$queery);
+$row = mysqli_fetch_assoc($results);
+if (!$results){
+die ('FAILED');
+}
 ?>
 <style>
 #form{
@@ -11,20 +19,20 @@ include 'assets/navbar.php';
     <br><br>
   <form action="/action_page.php" id="form" >
       <div class="form-group">
-        <label for="Name">Name:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter Name" name="email" value="">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" id="email" placeholder="Enter Name" name="name" value="<?php echo $row['name']?>">
       </div>
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" value="">
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" value="<?php echo $row['email']?>">
       </div>
       <div class="form-group">
-        <label for="Phone">Phone</label>
-        <input type="text" class="form-control" id="Phone" placeholder="Enter Phone number" name="Phone" value="">
+        <label for="phone">Phone</label>
+        <input type="text" class="form-control" id="phone" placeholder="Enter Phone number" name="phone" value="<?php echo $row['phone']?>">
       </div>
       <div class="form-group">
-        <label for="pwd">Date of admission:</label>
-        <input type="date" class="form-control" id="Dateofadmission" placeholder="Enter Date" name="Dateofadmission" value="">
+        <label for="date_of_admission">Date of admission:</label>
+        <input type="date" class="form-control" id="date_of_admission" placeholder="Enter Date" name="date_of_admission" value="<?php echo $row['date_of_admission']?>">
       </div>
       <div class="checkbox">
         <label><input type="checkbox" name="remember"> Are you sure</label>
