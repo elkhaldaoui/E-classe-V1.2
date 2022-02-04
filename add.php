f7ffd3d42af56bdbe55bfe3618b26b33cfa9f5fe
Oussama Elkhaldaoui <93930380+elkhaldaoui@users.noreply.github.com>
2022-02-04 19:53:55 +0100

    course page

--- add.php
+++ add.php
@@ -5,7 +5,6 @@ $connection = mysqli_connect ("localhost","root","","e_classe_db");
 $queery = "SELECT * FROM payment_details WHERE id=$id ";
 $results = mysqli_query($connection,$queery);
 $row = mysqli_fetch_assoc($results);
-
 ?>
 <style>
 #form{
@@ -18,32 +17,30 @@ $row = mysqli_fetch_assoc($results);
   <form action="/action_page.php" id="form" >
       <div class="form-group">
         <label for="name">Name:</label>
-        <input type="text" class="form-control" id="name" placeholder="" name="name" value="<?php echo $row['name']?>">
+        <input type="text" class="form-control" id="name" placeholder="" name="name" value="<?php echo $row['name']?>" disabled>
       </div>
       <div class="form-group">
         <label for="payment_schedule">Payment schedule:</label>
-        <input type="payment_schedule" class="form-control" id="payment_schedule" placeholder="" name="payment_schedule" value="<?php echo $row['payment_schedule']?>">
+        <input type="payment_schedule" class="form-control" id="payment_schedule" placeholder="" name="payment_schedule" value="<?php echo $row['payment_schedule']?>" disabled>
       </div>
       <div class="form-group">
         <label for="bill_number">Bill Number</label>
-        <input type="text" class="form-control" id="bill_number" placeholder="" name="bill_number" value="<?php echo $row['bill_number']?>">
+        <input type="text" class="form-control" id="bill_number" placeholder="" name="bill_number" value="<?php echo $row['bill_number']?>" disabled>
       </div>
       <div class="form-group">
         <label for="amount_paid">Amount Paid</label>
-        <input type="text" class="form-control" id="amount_paid" placeholder="" name="amount_paid" value="<?php echo $row['amount_paid']?>">
+        <input type="text" class="form-control" id="amount_paid" placeholder="" name="amount_paid" value="<?php echo $row['amount_paid']?>" disabled>
       </div>
       <div class="form-group">
         <label for="balance_amount">Balance Amount</label>
-        <input type="text" class="form-control" id="balance_amount" placeholder="" name="balance_amount" value="<?php echo $row['balance_amount']?>">
+        <input type="text" class="form-control" id="balance_amount" placeholder="" name="balance_amount" value="<?php echo $row['balance_amount']?>" disabled>
       </div>
       <div class="form-group">
         <label for="date">Date:</label>
-        <input type="date" class="form-control" id="date" placeholder="" name="date" value="<?php echo $row['date']?>">
+        <input type="text" class="form-control" id="date" placeholder="" name="date" value="<?php echo $row['date']?>"disabled>
       </div>
-      <div class="checkbox">
-        <label><input type="checkbox" name="remember"> Are you sure</label>
-      </div>
-    <a class="btn btn-primary" href="payment.php" role="button">Submit</a>
+     <br>
+    <a class="btn btn-primary" href="payment.php" role="button">GO BACK</a>
   </form>
 </div>
 
--- course.php
+++ course.php
@@ -1,33 +1,14 @@
 <?php
 include 'assets/navbar.php';
+$connection = mysqli_connect ("localhost","root","","e_classe_db");
+$queery = "SELECT * FROM coures ";
+$results = mysqli_query($connection,$queery);
+while($row = mysqli_fetch_assoc($results)){
 ?>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
+}
 include './js.php';
 ?>
